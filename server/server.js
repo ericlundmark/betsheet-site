ae599acef8a028d0a526896a0637b7bb0e51ba68
Eric Lundmark <erilu506@gmail.com>
2014-08-12 20:37:20 +0200

    updated

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+Meteor.startup(function () {
+    if (Meteor.users.find().count() === 0) {
+	seedUserId = Accounts.createUser({
+	    email: 'erilu506@gmail.com',
+	    password: '123456'
+	});
+    }
+});
 Meteor.publish("sheetsByUserId", function(userId) {
     return Sheets.find({"author._id":userId},{sort:{lastAdded:-1}});
 });
